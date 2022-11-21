d7417cedb7990a7188a11318d4ba50a68bc70708
rasedprogrammer <rasedprogrammer@gmail.com>
2022-11-22 00:31:48 +0600

    adding router redirects for router redirect issue

--- src/components/QuizQuestionsDetails/QuizQuestionDetails.js
+++ src/components/QuizQuestionsDetails/QuizQuestionDetails.js
@@ -1,3 +1,5 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import OptionBox from "../OptionBox/OptionBox";
 import "./QuizQuestionDetails.css";
@@ -7,7 +9,12 @@ const QuizQuestionDetails = ({ quizQuestion }) => {
 	console.log(quizQuestion);
 	return (
 		<div className="quizQuestionDetailsContainer">
-			<h2>{question}</h2>
+			<div className="title-bar">
+				<h2>{question}</h2>
+				<button className="btn-eye">
+					<FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+				</button>
+			</div>
 			<div className="option-box">
 				{options.map((option) => (
 					<OptionBox key={idx} option={option}></OptionBox>
